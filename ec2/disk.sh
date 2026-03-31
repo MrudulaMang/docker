7434add86e2566f0fe0b00c02da4e478d39c400a
mrudu <mrudu@example.com>
2026-03-31 12:41:13 +0530

    disk.sh ec2

--- ec2/disk.sh
+++ ec2/disk.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
 growpart /dev/nvme0n1 4 #grow full size
 pvresize /dev/nvme0n1p4 #i wrote coz lvextend may fail
-lvextend -r -L +30G /dev/mapper/RootVG-homeVol
+lvextend -r -L +30G /dev/mapper/RootVG-varVol
+xfs_growfs /var
 # xfx_growfs /home ----no need to use this command when using -r in the above
 
 #installing terraform inbastian to execute terra cmds to run ansible playbook in mongo thru terra to install mongodb database
