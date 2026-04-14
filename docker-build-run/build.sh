8104d1915eec19543bfc8fd2cf06ac8c9c3e4f9c
mrudu <mrudu@example.com>
2026-04-14 21:53:01 +0530

    docker-K8, ENV commented

---
+++ docker-build-run/build.sh
@@ -0,0 +1,33 @@
+cd d1-mongodb
+docker build -t mongodb:1.2.0
+docker run -d --name mongodb mongodb:1.2.0
+
+cd ../b1-catalogue
+docker build -t catalogue:1.2.0
+docker run -d --name catalogue catalogue:1.2.0
+
+cd ../b2-user
+docker build -t user:1.2.0
+docker run -d --name user user:1.2.0
+
+cd ../b3-cart
+docker build -t cart:1.2.0
+docker run -d --name cart cart:1.2.0
+
+cd ../b4-shipping
+docker build -t shipping:1.2.0
+docker run -d --name shipping shipping:1.2.0
+
+cd ../b5-payment
+docker build -t payment:1.2.0
+docker run -d --name payment payment:1.2.0
+cd ../b6-frontend
+docker build -t frontend:1.2.0
+docker run -d --name frontend frontend:1.2.0
+
+
+cd ../mysql
+docker build -t mysql:1.2.0
+docker run -d --name mysql mysql:1.2.0
+#rabbitmq
+#redis 
