18d1b581d50d79fdab8cccfed384ba244b6575f9
mrudu <mrudu@example.com>
2026-04-14 14:03:47 +0530

    docker

---
+++ d3-rabbitmq/init.sh
@@ -0,0 +1,9 @@
+# #!/bin/bash
+# rabbitmq-server &
+
+# sleep 10
+
+# rabbitmqctl add_user roboshop roboshop123
+# rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+
+# wait
--- dockerpull/pull.sh
+++ dockerpull/pull.sh
@@ -7,3 +7,4 @@ docker run -d --network roboshop --name cart mrudula158/cart:1.0.0
 docker run -d --network roboshop --name user mrudula158/user:1.0.0
 docker run -d --network roboshop --name shipping mrudula158/shipping:1.0.0
 docker run -d --network roboshop --name payment mrudula158/payment:1.0.0
+docker run -d -p 80:80 --network roboshop --name frontend mrudula158/frontend:1.0.0
